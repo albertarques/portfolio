f9d38a241c0e5225cfd81a14cec454fa477a434f
Albert Arques <albertarques@gmail.com>
2023-04-30 09:06:04 +0200

    Added Button component

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({children}) {
   return (
-    <button className='border rounded-2xl m-3 py-2 px-4'>{children}</button>
+    <button className='border-f rounded-2xl m-3 py-2 px-4'>{children}</button>
   )
 }
 
