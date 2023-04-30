6d9253d51f8337830074e3f03e0ba4b651e77823
Albert Arques <albertarques@gmail.com>
2023-04-30 09:06:18 +0200

    Added Hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,9 @@
 export default function Hero({title, subtitle, subtitle2}) {
   return (
-    <div className="container flex-col flex px-5 h-screen justify-center">
+    <section className="container flex-col flex px-5 py-52 justify-center">
       <h1 className="text-3xl lg:text-5xl font-bold m-3">{title}</h1>
       <p className="text-2xl lg:text-4xl m-3">{subtitle}</p>
       <p className="text-2xl lg:text-3xl m-3">{subtitle2}</p>
-    </div>
+    </section>
   )
 }
